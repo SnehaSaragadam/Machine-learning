9a78584af0873b20cf454b1e11a97f9719f5853a
SnehaSaragadam <137038530+SnehaSaragadam@users.noreply.github.com>
2024-03-12 04:44:26 +0530

    Update A3.py

--- A3.py
+++ A3.py
@@ -4,10 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 df = pd.read_excel('Lab Session1 Data.xlsx', sheet_name='Purchase Data')
-df['Class'] = np.where(df['Payments'] > 200, 'RICH', 'POOR')
+df['Class'] = np.where(df['Payments'] > 200, 'RICH', 'POOR')#adding a new column class with values as rich if payments more than 200 else poor 
 X = df.iloc[:, 1:4] 
 y = df['Class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)#data set split size 80-20
 model = LogisticRegression()
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
